815e30b7a3807c6399263355788092d2e0aac0e3
Vitormanoel17 <vitormanoel.aquino@gmail.com>
2023-04-26 13:28:46 -0300

    Adicionando cores para identificar a qual jogador o território pertence

--- war/src/app/homepage/sidebar/sidebar.component.ts
+++ war/src/app/homepage/sidebar/sidebar.component.ts
@@ -11,14 +11,54 @@ import { Mapa } from 'src/app/interface/Mapa';
 export class SidebarComponent implements OnInit {
   
   mapa !: Mapa;
+  player : number [] = [1,2,3,4,5,6];
+  ownerTerritory : { ownerId: number, territoryId: number } [] = [{ownerId:1,territoryId:1}];
 
   ngOnInit() : void {
     console.log(map)
 
     this.mapa = map;
+    this.distribuiton();
   }
 
   checkMap(territorio : string) : void {
     window.alert(`Territorio Selecionado: ${territorio}`)
+  }
+
+  colorTerritory(territoryId : number) : string {
+    
+    const playerId = this.ownerTerritory.find(territory => territory.territoryId == territoryId);
+    console.log(territoryId, playerId)
+
+    const player1 = this.ownerTerritory.filter(territory => territory.ownerId == 1);
+
+    console.log(player1.length)
+
+    switch (playerId?.ownerId) {
+      case 1:
+        return 'blue';
+      case 2:
+        return 'orange';
+      case 3:
+        return 'yellow';
+      case 4:
+        return 'purple';
+      case 5:
+        return 'gray';
+      case 6:
+        return 'green';
+      default:
+        return 'black';
+    }
+  }
+
+  distribuiton() : void {
+    this.mapa.continents.forEach(continent => {
+      continent.regions.forEach(regions => {
+        this.ownerTerritory.push({ownerId: this.player[Math.floor(Math.random()*5)], territoryId: regions.id})
+      })
+
+    })
+    console.log(this.ownerTerritory)
   }
 }
